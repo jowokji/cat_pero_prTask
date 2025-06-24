53616d8a9d7c34f7c73a771a601068f51854d043
MIkita Dubrovin <115701206+jowokji@users.noreply.github.com>
2025-06-24 19:38:59 +0300

    Delete cat_pero_kot-main_fixed directory
    unworked test version

--- cat_pero_kot-main_fixed/app/src/main/java/com/example/network/ui/state/CatUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.network.ui.state
-
-import com.example.network.data.model.CatImageModel
-
-data class CatUiState(
-    val cats: List<CatImageModel> = emptyList(),
-    val isLoading: Boolean = false,
-    val errorMessage: String? = null
-)
